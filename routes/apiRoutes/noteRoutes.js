4150407ccb0ecbafdddaa646f72e9fb431107c09
swvmpdad <will.g.camp@gmail.com>
2022-08-12 20:40:13 -0500

    finished get and post routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { notes } = require('../../data/notes.json');
+const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
@@ -7,8 +8,9 @@ router.get('/notes', (req, res) => {
     res.json(results);
 });
 
-// router.post('/notes', (req, res) => {
-    
-// });
+router.post('/notes', (req, res) => {
+   const note = createNewNote(req.body, notes);
+   res.json(note); 
+});
 
 module.exports = router;
