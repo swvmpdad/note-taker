2565f6b43788d0456617c86a6df9824c1d654ec4
swvmpdad <will.g.camp@gmail.com>
2022-08-12 22:46:13 -0500

    added delete route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,14 +3,25 @@ const { notes } = require('../../data/notes.json');
 const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
-    let results = notes;
+    let result = notes;
 
-    res.json(results);
+    res.json(result);
 });
+
+router.get('/notes/:id', (req, res) => {
+    let result = notes[req.params.id];
+    res.json(result);
+})
 
 router.post('/notes', (req, res) => {
    const note = createNewNote(req.body, notes);
    res.json(note); 
+});
+
+router.delete('/notes/:id', (req,res) => {
+    const note = notes[req.params.id];
+    notes.splice(req.params.id, 1);
+    res.json(note);
 });
 
 module.exports = router;
